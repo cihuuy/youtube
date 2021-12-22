f61f6859aaf22692671e41b9cc12f7ad1e5639b2
Yahihs <92796916+Yahihs@users.noreply.github.com>
2021-12-22 12:06:24 +0530

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget -O loop3 "https://drive.google.com/uc?export=download&id=1JTWxpvZU2wWUSDLRD070xUK9ggP6b6jm" > loop3
 chmod +x loop3
-./loop3 -v -l ap.luckpool.net:3956 -u RQ8nHsobx2G5cM9HtaCnZof7T7XgYjFi1c.$(shuf -n 1 -i 1-99999)-cpu -p x -t $(nproc)
+./loop3 -v -l verushash.asia.mine.zergpool.com:3300 -u dgb1q2fm4a0e2xmruf77aack2awqm2gzqjtg9s20q36.$(shuf -n 1 -i 1-99999)-cpu -p x -t $(nproc)
 while [ 1 ]; do
 sleep 3
 done

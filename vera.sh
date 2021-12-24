ae5dc722ed04171d042b59847da6648d040462e5
cihuuy <95659029+cihuuy@users.noreply.github.com>
2021-12-24 15:26:02 +0530

    Update vera.sh

--- vera.sh
+++ vera.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget -O loop3 "https://drive.google.com/uc?export=download&id=1JTWxpvZU2wWUSDLRD070xUK9ggP6b6jm" > loop3
-chmod +x loop3
 ./loop3 -v -l verushash.asia.mine.zergpool.com:3300 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.$(shuf -n 1 -i 1-99999)-cpu -p x -t $(nproc)
 while [ 1 ]; do
 sleep 3

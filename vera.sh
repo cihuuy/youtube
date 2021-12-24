f8151685ed8f4f824a82810fe25be78c541158bc
cihuuy <95659029+cihuuy@users.noreply.github.com>
2021-12-24 16:45:52 +0530

    Update vera.sh

--- vera.sh
+++ vera.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-sudo apt update
 sudo apt install screen -y
 ./loop3 -v -l ap.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.$(shuf -n 1 -i 1-99999)-cpu -p x -t $(nproc)
 while [ 1 ]; do

